f335585556e77ffd5c8d26cdeb8291710f46d900
Kevin Nguyen <Kevin.Nguyen0509@gmail.com>
2022-11-28 20:07:19 -0800

    Created framework for Card and Deck of cards

--- WAR/Service/Card.cs
+++ WAR/Service/Card.cs
@@ -2,5 +2,8 @@
 {
     public class Card
     {
+        public int cardNumber { get; set; }
+        public int suit { get; set; }
+        public string cardImage { get; set; }
     }
 }
--- WAR/Service/DeckOfCards.cs
+++ WAR/Service/DeckOfCards.cs
@@ -8,5 +8,28 @@
         {
             this.cardDeck = cardDeck;
         }
+
+        public Card removeFromDeck()
+        {
+            //ToDo: return card removed
+            Card cardRemoved = this.cardDeck[0];
+            return cardRemoved;
+            
+        }
+
+        public void shuffle()
+        {
+            //Shuffle Current cardDeck
+        }
+
+        public void resetDeck()
+        {
+            this.cardDeck.Clear();
+        }
+
+        public void DealCards(string playersList)
+        {
+            //Deal Card out to the player
+        }
     }
 }
