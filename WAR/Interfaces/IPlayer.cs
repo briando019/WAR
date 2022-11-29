9a66c8dd3c77946d5519c8b8fc3d2f1806fbb95e
bpgor <brianado@msn.com>
2022-11-28 20:08:29 -0800

    Built framework for IPlayer. Connect interface for Computer and Player.

--- WAR/Interfaces/IPlayer.cs
+++ WAR/Interfaces/IPlayer.cs
@@ -1,7 +1,31 @@
-﻿namespace WAR.Interfaces
+﻿using WAR.Service;
+
+namespace WAR.Interfaces
 {
     public interface IPlayer
     {
+        public List<Card> PlayerHand { get; set; }
+        public int score { get; set; }
+        public int beenSlapped { get; set; }
+        public int blockedSlaps { get; set; }
+
+        public Card DrawCard(DeckOfCards deckOfCards);
+
+        public List<Card> LookAtHand();
+
+        public int PickCard(int selectedCardFromHand);
+
+        public int SlapAttack(int slapAttackInt);
+        public int BlockSlap(int blockInt);
+        public int getSlapCount();
+        public int getSlapBlockCount();
+        public void resetPlayerHand();
+
         
+
+            
+                
+            
+
     }
 }
---
+++ WAR/Service/Player.cs
@@ -0,0 +1,53 @@
+﻿using WAR.Interfaces;
+
+namespace WAR.Service
+{
+    public class Player : IPlayer
+
+    {
+        public List<Card> PlayerHand { get; set; }
+        public int score { get; set; }
+        public int beenSlapped { get; set; }
+        public int blockedSlaps { get; set; }
+
+        public int BlockSlap(int blockInt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Card DrawCard(DeckOfCards deckOfCards)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getSlapBlockCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int getSlapCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Card> LookAtHand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int PickCard(int selectedCardFromHand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void resetPlayerHand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SlapAttack(int slapAttackInt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
